0e621581ca0ed2546d4e6daa4cdcb96caf125b4c
Aviv Cohn <acohn@dgitech.com>
2023-12-13 10:01:51 -0500

    neater

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import os
 
 folderNamesToChange = ["Control", "GUI", "UI"]
-
+renameLog = []
+counter = 0
 
 def rename_control_folder(folderPath):
-  logMessage = ""
-  counter = 0
-  renameLog = []
   for root, directories, files in os.walk(folderPath):
     for directory in directories:
       if directory in folderNamesToChange:
         old_directory = directory
-        print(old_directory)
         old_path = os.path.join(root, directory)
         parent_name = os.path.basename(root)
         new_path = os.path.join(root, f"{parent_name}.{old_directory}")
         os.rename(old_path, new_path)
-        counter += 1
-        logMessage = f"Renamed folder '{old_path}' to '{new_path}'"
-        print(logMessage)
-        print(folderPath+"\log.txt")
-        renameLog.append(f"{counter}: {logMessage}")
-        WriteLog(renameLog, folderPath+"\renamelog.txt")
+        LogStuff(old_path, new_path, folderPath)
+
 
   print(f'{counter} folders renamed')
 
+def LogStuff(old, new, path):
+  global counter
+  logMessage = ""
+  counter += 1
+  logMessage = f"Renamed folder '{old}' to '{new}'"
+  print(logMessage)
+  print(path + "\log.txt")
+  renameLog.append(f"{counter}: {logMessage}")
+  WriteLog(renameLog, path + r"\renamelog.txt")
 
 def WriteLog(list, fileName):
   with open(fileName, "w") as file: #'w' is the write mode
