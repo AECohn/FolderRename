e22dfb5bdb19b75fc6438a2c30636f8a393a8783
Aviv Cohn <acohn@dgitech.com>
2023-12-14 12:25:36 -0500

    List is changable

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ foldernamestochange = ["test", "ui"]
 
 # To-Do: Program should initially build a list of all folders that will be renamed,
 # the user can then enter an index id to remove that index from the list, and then after confirming, the remaining folders in the list will be renamed
-def buildrenamelist(folderpath, foldernames):
+def build_rename_list(folderpath, foldernames):
     folder_names = []
-    counter = 0
     for root, directories, files in os.walk(folderpath):
         for directory in directories:
             if directory in foldernames:
-                old_path = directory
-                oldpath = os.path.join(root, directory)
+                old_path = os.path.join(root, directory)
                 parentname = os.path.basename(root)
-                new_path = os.path.join(root, f"{parentname}.{old_path}")
-                counter += 1
-                #logmessage = f"'{old_path}' will be changed to '{new_path}'"
+                new_path = os.path.join(root, f"{parentname}.{directory}")
                 folder_names.append((old_path, new_path))
     return folder_names
 
@@ -32,8 +28,9 @@ def writelog(folderlist, filename):
         for logValue in folderlist:
             file.write(f"{logValue}\n")
 
+#User Stuff
 startDir = input("what directory should we use?")
-templist = buildrenamelist(startDir, foldernamestochange)
+templist = build_rename_list(startDir, foldernamestochange)
 while True:
     for index, folder in enumerate(templist):
         print(f"{index+1} {folder[0]} will be renamed to {folder[1]}")
@@ -41,9 +38,10 @@ while True:
     if response != 'y':
         if response.isalnum():
             templist.remove(templist[int(response)-1])
-    #else:
-        #for folder in templist:
-        ##os.rename(oldpath, newpath)
+    else:
+        for folder in templist:
+            os.rename(folder[0], folder[1])
+        break
 
 #def renamefolders(folderlist):
     #for folder in folderlist:
