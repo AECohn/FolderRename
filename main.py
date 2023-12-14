f1465d78acd11e37c60d97dd05e73bb17cf60756
Aviv Cohn <acohn@dgitech.com>
2023-12-14 10:58:32 -0500

    switching to tuples, and giving user ability to remove items from the list

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import os
 
-folderNamesToChange = []
-renameLog = []
-counter = 0
+foldernamestochange = ["test", "ui"]
 
 
 # To-Do: Program should initially build a list of all folders that will be renamed,
 # the user can then enter an index id to remove that index from the list, and then after confirming, the remaining folders in the list will be renamed
-def renamecontrolfolder(folderpath, foldernames):
+def buildrenamelist(folderpath, foldernames):
+    folder_names = []
+    counter = 0
     for root, directories, files in os.walk(folderpath):
         for directory in directories:
             if directory in foldernames:
-                old_directory = directory
-                old_path = os.path.join(root, directory)
-                parent_name = os.path.basename(root)
-                new_path = os.path.join(root, f"{parent_name}.{old_directory}")
-                os.rename(old_path, new_path)
-                logstuff(old_path, new_path, folderpath)
-    print(f'{counter} folders renamed')
+                old_path = directory
+                oldpath = os.path.join(root, directory)
+                parentname = os.path.basename(root)
+                new_path = os.path.join(root, f"{parentname}.{old_path}")
+                counter += 1
+                #logmessage = f"'{old_path}' will be changed to '{new_path}'"
+                folder_names.append((old_path, new_path))
+    return folder_names
 
 
-def logstuff(old, new, path):
+'''def buildList(old, new, path):
     global counter
-    counter += 1
-    logmessage = f"Renamed folder '{old}' to '{new}'"
-    print(logmessage)
-    print(path + "\log.txt")
-    renameLog.append(f"{counter}: {logmessage}")
-    writelog(renameLog, path + r"\renamelog.txt")
+    
+    writelog(renamelist, path + r"\renamelog.txt")'''
 
 
 def writelog(folderlist, filename):
@@ -35,6 +32,18 @@ def writelog(folderlist, filename):
         for logValue in folderlist:
             file.write(f"{logValue}\n")
 
-
 startDir = input("what directory should we use?")
-renamecontrolfolder(startDir, folderNamesToChange)
+templist = buildrenamelist(startDir, foldernamestochange)
+while True:
+    for index, folder in enumerate(templist):
+        print(f"{index+1} {folder[0]} will be renamed to {folder[1]}")
+    response = input("to remove an index, enter the number of the index to remove, otherwise press 'y' to rename")
+    if response != 'y':
+        if response.isalnum():
+            templist.remove(templist[int(response)-1])
+    #else:
+        #for folder in templist:
+        ##os.rename(oldpath, newpath)
+
+#def renamefolders(folderlist):
+    #for folder in folderlist:
